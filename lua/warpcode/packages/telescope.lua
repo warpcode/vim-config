ee52df2ae9b9301246ab560974bfb168a43358c9
Warpcode <git@warpcode.co.uk>
2021-11-27 13:45:44 +0000

    Restore behaviour where buffers are ordered by last used

--- lua/warpcode/packages/telescope.lua
+++ lua/warpcode/packages/telescope.lua
@@ -20,13 +20,10 @@ require('telescope').setup{
     }
   },
   pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
+      buffers = {
+            ignore_current_buffer = true,
+            sort_lastused = true,
+        },
   },
   extensions = {
     fzf = {
