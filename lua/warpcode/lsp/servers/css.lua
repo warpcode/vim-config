3074ee761bea23d49bb67c37aeca466fa8330e4d
Warpcode <git@warpcode.co.uk>
2022-01-07 11:48:37 +0000

    Add css language server

--- lua/warpcode/lsp/servers/css.lua
+++ lua/warpcode/lsp/servers/css.lua
@@ -3,3 +3,20 @@
 -- require("lspconfig").cssls.setup(config({
 --     cmd = {vim.g.vim_source .. '/node_modules/.bin/vscode-css-language-server', '--stdio'},
 -- }))
+local packages = require('warpcode.utils.packages')
+
+if not packages.is_loaded('nvim-lspconfig') then
+    return
+end
+
+local config = require('warpcode.lsp.config')
+local lspconfig = require('lspconfig')
+local path = require('warpcode.utils.path')
+local bin_cssls = path.find_exe_path('vscode-css-language-server')
+
+
+if bin_cssls ~= '' then 
+    lspconfig.cssls.setup(config.common({
+        cmd = {bin_cssls, '--stdio'},
+    }))
+end
