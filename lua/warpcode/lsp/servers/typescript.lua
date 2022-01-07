600b69a0115ddb23bf924995d5afed0bbb25d1f8
Warpcode <git@warpcode.co.uk>
2022-01-07 11:33:02 +0000

    dd the necessary setup for the typescript/js language server

--- lua/warpcode/lsp/servers/typescript.lua
+++ lua/warpcode/lsp/servers/typescript.lua
@@ -1,2 +1,18 @@
+local packages = require('warpcode.utils.packages')
 
--- require("lspconfig").tsserver.setup(config())
+if not packages.is_loaded('nvim-lspconfig') then
+    return
+end
+
+local config = require('warpcode.lsp.config')
+local lspconfig = require('lspconfig')
+local path = require('warpcode.utils.path')
+local bin_tsserver = path.find_exe_path('typescript-language-server')
+
+
+if bin_tsserver ~= '' then 
+    -- lspconfig.tsserver.setup(config.common({
+    --     cmd = {bin_tsserver, '--stdio'},
+    -- }))
+end
+
