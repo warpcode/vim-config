4b600fafc25d87fa850051c11e81fc6ea2f78f17
Warpcode <git@warpcode.co.uk>
2022-01-08 22:12:31 +0000

    Re-enable null-ls, add fixjson and update some parameters

--- lua/warpcode/lsp/servers/null-ls.lua
+++ lua/warpcode/lsp/servers/null-ls.lua
@@ -5,30 +5,44 @@ if (not null_ls_status) then
 end
 
 local path = require('warpcode.utils.path')
+local attachments = require('warpcode.lsp.attachments')
 local bin_jsonlint = path.find_exe_path('jsonlint')
+local bin_fixjson = path.find_exe_path('fixjson')
 local bin_phpcbf = path.find_exe_path('phpcbf')
 local bin_phpcs = path.find_exe_path('phpcs')
 local bin_phpcsfixer = path.find_exe_path('php-cs-fixer')
 
 local sources = {
     -- JSON
-    -- null_ls.builtins.diagnostics.jsonlint.with({
-    --     command = bin_jsonlint,
-    -- }),
+    null_ls.builtins.diagnostics.jsonlint.with({
+        command = bin_jsonlint,
+    }),
+    null_ls.builtins.formatting.json_tool,
+    null_ls.builtins.formatting.fixjson.with({
+        command = bin_fixjson,
+    }),
 
     -- PHP
-    -- null_ls.builtins.diagnostics.php,
-    -- null_ls.builtins.formatting.phpcbf.with({
-    --     command = bin_phpcbf,
-    --     args = {'--standard=PSR12', '-'},
-    -- }),
-    -- null_ls.builtins.diagnostics.phpcs.with({
-    --     command = bin_phpcs,
-    --     args = {'--standard=PSR12', '--report=json', '-s', '-'},
-    -- }),
-    -- null_ls.builtins.formatting.phpcsfixer.with({
-    --     command = bin_phpcsfixer,
-    -- }),
+    null_ls.builtins.diagnostics.php,
+    null_ls.builtins.formatting.phpcbf.with({
+        command = bin_phpcbf,
+        extra_args = {
+            '--standard=PSR12',
+        },
+    }),
+    null_ls.builtins.diagnostics.phpcs.with({
+        command = bin_phpcs,
+        extra_args = {
+            '--standard=PSR12', 
+        },
+    }),
+    null_ls.builtins.formatting.phpcsfixer.with({
+        command = bin_phpcsfixer,
+    }),
 }
 
-null_ls.setup({ sources = sources })
+null_ls.setup({
+    debug = false,
+    sources = sources,
+    on_attach = attachments.common,
+})
