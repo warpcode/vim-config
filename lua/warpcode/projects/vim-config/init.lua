32a6c314df61b006f3f745efa3b31ff926c37ac5
Warpcode <warpcodeuk@gmail.com>
2022-01-23 19:56:45 +0000

    Add vim-config project

---
+++ lua/warpcode/projects/vim-config/init.lua
@@ -0,0 +1,21 @@
+local Base = require('warpcode.projects.base')
+
+local Martini = Base:new()
+
+--- Construct a new instance of the class
+---@param buffnr int
+function Martini:new(buffnr)
+    local o = Base:new(buffnr)
+
+    setmetatable(o, self)
+    self.__index = self
+    self._project_name = 'Vim Config'
+    self._project_name_slug = 'vim-config'
+    self._root_detection = 'files'
+    self._root_detection_type = 'strict'
+    self._root_base_files = {'init.vim', 'vimrc'}
+
+    return o
+end
+
+return Martini
