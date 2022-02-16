fb225292f638c7596fc0b09579918fd7f9817a4c
Warpcode <warpcodeuk@gmail.com>
2022-02-16 15:50:50 +0000

    Setting up some new keymaps for lsp

--- lua/warpcode/lsp/keymaps.lua
+++ lua/warpcode/lsp/keymaps.lua
@@ -4,10 +4,6 @@ M.common = function(client, bufnr)
     local opts = {noremap = true, silent = true, buffer = bufnr}
 
     -- Mappings.
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     -- buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
     -- buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
     -- buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
@@ -15,6 +11,10 @@ M.common = function(client, bufnr)
     -- buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
     -- buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
     -- buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    vim.keymap.set("n", "gD", lbuf.declaration, opts)
+    vim.keymap.set("n", "gd", lbuf.definition, opts)
+    vim.keymap.set("n", "K", lbuf.hover, opts)
+    vim.keymap.set("n", "gi", lbuf.implementation, opts)
     -- buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
@@ -22,11 +22,15 @@ M.common = function(client, bufnr)
     -- buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 
     -- Set some keybinds conditional on server capabilities
-    -- if client.resolved_capabilities.document_formatting then
-    --     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    -- elseif client.resolved_capabilities.document_range_formatting then
-    --     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-    -- end
+    if client.resolved_capabilities.document_formatting then
+        vim.keymap.set("n", "<leader>=", lbuf.formatting_seq_sync, opts)
+    elseif client.resolved_capabilities.document_range_formatting then
+        vim.keymap.set("n", "<leader>=", lbuf.range_formatting, opts)
+    end
+
+    if client.resolved_capabilities.document_range_formatting then
+        vim.keymap.set("x", "<leader>=", lbuf.range_formatting, opts)
+    end
 end
 
 return M
