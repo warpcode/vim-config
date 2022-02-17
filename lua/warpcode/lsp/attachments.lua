bef44f6a8a47f3ae335894c211c379cc70a80d32
Warpcode <warpcodeuk@gmail.com>
2022-02-17 12:39:45 +0000

    Enable highlighting on hover

--- lua/warpcode/lsp/attachments.lua
+++ lua/warpcode/lsp/attachments.lua
@@ -7,6 +7,20 @@ M.common = function (client, bufnr)
 
     keymaps.common(client, bufnr)
 
+    -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_highlight then
+        vim.api.nvim_exec([[
+          " hi LspReferenceRead cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+          " hi LspReferenceText cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+          " hi LspReferenceWrite cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+          augroup lsp_document_highlight
+              autocmd! * <buffer>
+              autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+              autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+          augroup END
+        ]], false)
+    end
+
     -- Initialise plugins
     lsp_signature.on_attach(client)
 end
