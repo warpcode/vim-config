832ec071a914e63ff6567c738a35323df1c836b3
Warpcode <warpcodeuk@gmail.com>
2022-02-18 15:45:13 +0000

    Update globals

--- lua/warpcode/globals.lua
+++ lua/warpcode/globals.lua
@@ -1,12 +1,19 @@
-_G.warpcode = {
-    print = function (...)
-        local objects = {}
-        for i = 1, select('#', ...) do
-            local v = select(i, ...)
-            table.insert(objects, vim.inspect(v))
-        end
+_G.P = function (...)
+    local objects = {}
+    for i = 1, select('#', ...) do
+        local v = select(i, ...)
+        table.insert(objects, vim.inspect(v))
+    end
 
-        print(table.concat(objects, '\n'))
-        return ...
-    end,
-}
+    print(table.concat(objects, '\n'))
+    return ...
+end
+
+_G.RELOAD = function (...)
+    return require 'plenary.reload'.reload_module(...)
+end
+
+_G.R = function (name)
+    RELOAD(name)
+    return require(name)
+end
