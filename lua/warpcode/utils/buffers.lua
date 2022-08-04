35045953021689aee45bff72cef2addae0ecba01
Warpcode <warpcodeuk@gmail.com>
2022-08-04 16:37:15 +0100

    Some cleanup

--- lua/warpcode/utils/buffers.lua
+++ lua/warpcode/utils/buffers.lua
@@ -3,13 +3,14 @@ local M = {}
 M.get_bufnr = function(bufnr)
     if type(buffnr) == 'number' and buffnr > 0 then
         return buffnr
-    else
-        return vim.api.nvim_get_current_buf()
     end
+
+    return vim.api.nvim_get_current_buf()
 end
 
 M.get_file_types = function (buf)
-    local buffer_filetype = vim.api.nvim_buf_get_option(buf or 0, 'filetype')
+    buf = buf or 0
+    local buffer_filetype = vim.api.nvim_buf_get_option(buf, 'filetype')
     local filetypes = vim.split(buffer_filetype, '.', true)
 
     return filetypes
