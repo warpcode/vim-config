5339373e60668030ec30b328fdc7b702bacfdb6c
Warpcode <warpcodeuk@gmail.com>
2022-09-14 22:29:56 +0100

    feat: moving more keymaps to lua

--- lua/warpcode/keymaps/init.lua
+++ lua/warpcode/keymaps/init.lua
@@ -4,6 +4,7 @@ local maps = {
     require(lib_prefix .. 'clipboard'),
     require(lib_prefix .. 'history'),
     require(lib_prefix .. 'misc'),
+    require(lib_prefix .. 'navigation'),
     require(lib_prefix .. 'search'),
     require(lib_prefix .. 'text'),
     require(lib_prefix .. 'ui'),
---
+++ lua/warpcode/keymaps/navigation.lua
@@ -0,0 +1,24 @@
+return {
+    -- Buffers
+    {'n', '<leader>bb', function()
+        require('telescope.builtin').buffers()
+    end},
+    {'n', '<leader>bp', ':bp<CR>'},
+    {'n', '<leader>bn', ':bn<CR>'},
+    {'n', '<leader>bc', ':bd<CR>'},
+    {'n', '<leader>bq', ':bp <BAR> bd #<cr>'},
+
+    -- File Explorer
+    {'n', '<leader>nw', ':Lexplore<CR>'},
+
+    -- File Finder
+    {'n', '<leader>ff', function()
+        require('warpcode.plugins.telescope.finders').project_files()
+    end},
+    -- command! -nargs=1 FindFile call warpcode#search#findFiles(<q-args>)
+
+    -- move vertically by visual line (ie will go into a wrapped line that's
+    -- visually on the next line)
+    {'n', 'j', 'gj'},
+    {'n', 'k', 'gk'},
+}
