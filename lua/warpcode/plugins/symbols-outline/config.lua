b191e3c54fc081cfa362fc158d958af8642af87b
Warpcode <warpcodeuk@gmail.com>
2022-09-30 17:27:58 +0100

    feat: better handling for plugin config for symbols-outline
    This will be the standard for other modules

--- lua/warpcode/plugins/symbols-outline/config.lua
+++ lua/warpcode/plugins/symbols-outline/config.lua
@@ -2,21 +2,21 @@ local symbols_outline_ok, symbols_outline = pcall(require, 'symbols-outline')
 
 return {
     run = function()
-        if not symbols_outline_ok then return end
+        pcall(function()
+            local opts = {
+                -- whether to highlight the currently hovered symbol
+                -- disable if your cpu usage is higher than you want it
+                -- or you just hate the highlight
+                -- default: true
+                highlight_hovered_item = true,
 
-        local opts = {
-            -- whether to highlight the currently hovered symbol
-            -- disable if your cpu usage is higher than you want it
-            -- or you just hate the highlight
-            -- default: true
-            highlight_hovered_item = true,
+                -- whether to show outline guides
+                -- default: true
+                show_guides = true,
+            }
 
-            -- whether to show outline guides
-            -- default: true
-            show_guides = true,
-        }
+            require('symbols-outline').setup(opts)
+        end)
 
-        symbols_outline.setup(opts)
     end
 }
-
