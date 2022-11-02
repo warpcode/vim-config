4787d671e1fc71343708e710c0930d9bf3e947a0
Warpcode <warpcodeuk@gmail.com>
2022-11-02 11:18:28 +0000

    Update lsp capabilities

--- lua/warpcode/lsp/capabilities.lua
+++ lua/warpcode/lsp/capabilities.lua
@@ -2,11 +2,11 @@ local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
 local M = {}
 
 M.common = function ()
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local capabilities = cmp_nvim_lsp.default_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-    if (cmp_nvim_lsp_status) then  
-        capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+    if (cmp_nvim_lsp_status) then
+        capabilities = capabilities
     end
 
 	return capabilities
