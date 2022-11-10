9b1ddc751a9e50c7c3c4e1c0755febb2d0387a42
Warpcode <warpcodeuk@gmail.com>
2022-11-10 16:23:17 +0000

    cleanup: clean up dap config a little

--- lua/warpcode/plugins/nvim-dap/config.lua
+++ lua/warpcode/plugins/nvim-dap/config.lua
@@ -1,28 +1,30 @@
 return {
     run = function()
         pcall(function()
-            dap = require 'dap'
+            local dap = require "dap"
+            local widgets = require "dap.ui.widgets"
+            local debug = require "debugHelper"
 
             vim.fn.sign_define('DapBreakpoint', {text='🔴', texthl='', linehl='', numhl=''})
             vim.fn.sign_define('DapBreakpointRejected', {text='❌', texthl='', linehl='', numhl=''})
             vim.fn.sign_define('DapStopped', {text='⭐️', texthl='', linehl='', numhl=''})
 
-            vim.keymap.set('n', '<leader>db', function() require"dap".toggle_breakpoint() end)
+            vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end)
             vim.keymap.set('n', '<leader>dB', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
-            vim.keymap.set('n', '<leader>dc', function() require"dap".continue() end)
-            vim.keymap.set('n', '<leader>dC', function() require"dap".terminate() end)
-            vim.keymap.set('n', '<leader>do', function() require"dap".step_over() end)
-            vim.keymap.set('n', "<leader>di", function() require"dap".step_into() end)
-            vim.keymap.set('n', '<leader>dI', function() require"dap".step_out() end)
+            vim.keymap.set('n', '<leader>dc', function() dap.continue() end)
+            vim.keymap.set('n', '<leader>dC', function() dap.terminate() end)
+            vim.keymap.set('n', '<leader>do', function() dap.step_over() end)
+            vim.keymap.set('n', "<leader>di", function() dap.step_into() end)
+            vim.keymap.set('n', '<leader>dI', function() dap.step_out() end)
             vim.keymap.set('n', '<leader>dk', ':lua require"dap".up()<CR>zz')
             vim.keymap.set('n', '<leader>dj', ':lua require"dap".down()<CR>zz')
-            -- vim.keymap.set('n', '<leader>dn', function() require"dap".run_to_cursor() end)
-            -- vim.keymap.set('n', '<leader>dR', function() require"dap".clear_breakpoints() end)
-            -- vim.keymap.set('n', '<leader>de', function() require"dap".set_exception_breakpoints({"all"}) end)
-            vim.keymap.set('n', '<leader>da', function() require"debugHelper".attach() end)
-            vim.keymap.set('n', '<leader>dA', function() require"debugHelper".attachToRemote() end)
-            vim.keymap.set('n', '<leader>dh', function() require"dap.ui.widgets".hover() end)
-            vim.keymap.set('n', '<leader>d?', function() local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes) end)
+            -- vim.keymap.set('n', '<leader>dn', function() dap.run_to_cursor() end)
+            -- vim.keymap.set('n', '<leader>dR', function() dap.clear_breakpoints() end)
+            -- vim.keymap.set('n', '<leader>de', function() dap.set_exception_breakpoints({"all"}) end)
+            vim.keymap.set('n', '<leader>da', function() debug.attach() end)
+            vim.keymap.set('n', '<leader>dA', function() debug.attachToRemote() end)
+            vim.keymap.set('n', '<leader>dh', function() widgets.hover() end)
+            vim.keymap.set('n', '<leader>d?', function() widgets.centered_float(widgets.scopes) end)
             vim.keymap.set('n', '<leader>dr', ':lua require"dap".repl.toggle({}, "vsplit")<CR><C-w>l')
 
             local vscode_debugger_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/vscode-php-debug/out/phpDebug.js'
