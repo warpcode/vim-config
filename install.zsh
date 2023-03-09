691a91fda60fe5514ba09edf150af1e8ffb5c95b
Warpcode <warpcodeuk@gmail.com>
2023-03-09 21:21:10 +0000

    Temporary install script

--- install.zsh
+++ install.zsh
@@ -6,6 +6,19 @@ pushd ${0:A:h}
 # Ensure dependencies are up to date
 git submodule update --init
 
+# Ensure neovim is installed
+if (( ! $+commands[nvim] )); then
+    popd
+    echo "neovim is not installed"
+    exit 1
+fi
+
+if (( ! $+commands[make] )); then
+    popd
+    echo "make is not installed"
+    exit 1
+fi
+
 # Ensure npm is installed and run
 if (( $+commands[npm] )); then
     [ -f package-lock.json ] && npm update
@@ -25,6 +38,7 @@ else
     exit 1
 fi
 
+make install-nvim
 
 echo "Install complete"
 echo "You must sylink bin/wvim to a bin directory in PATH"
