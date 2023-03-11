a98060717a6067833f4f9b5bfc7211b9ec7ec010
Warpcode <warpcodeuk@gmail.com>
2023-03-11 14:20:42 +0000

    Config tidy

--- lua/warpcode/plugins/undotree/init.lua
+++ lua/warpcode/plugins/undotree/init.lua
@@ -1,7 +1,13 @@
 local M = {
     source = 'mbbill/undotree',
     config = function()
-        require 'warpcode.plugins.undotree.config'.run()
+        vim.api.nvim_exec([[
+            let g:undotree_HighlightChangedWithSign = 0
+            let g:undotree_WindowLayout             = 4
+            let g:undotree_SetFocusWhenToggle       = 1
+
+            nnoremap <Leader>u :UndotreeToggle<CR>
+        ]], false)
     end
 }
 
