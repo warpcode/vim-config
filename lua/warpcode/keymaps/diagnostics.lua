543ce86b48f2fae1fd0bfc32275f6ae1f6685994
Warpcode <warpcodeuk@gmail.com>
2023-03-12 15:45:36 +0000

    Additional diagnostics keymaps

--- lua/warpcode/keymaps/diagnostics.lua
+++ lua/warpcode/keymaps/diagnostics.lua
@@ -1,8 +1,28 @@
 return {
 
-    { "n", "<leader>ds",  "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>" },
-    { "n", "[d",          vim.diagnostic.goto_prev, },
-    { "n", "]d",          vim.diagnostic.goto_next, },
-    { "n", "<leader>dll", vim.diagnostic.setloclist, },
-    { "n", "<leader>dqf", vim.diagnostic.setqflist, },
+    -- { "n", "<leader>ds", vim.diagnostic.open_float },
+    { "n", "[d", vim.diagnostic.goto_prev, },
+    { "n", "]d", vim.diagnostic.goto_next, },
+    { "n", "<leader>dll", function()
+        local status = pcall(function()
+            require('telescope.builtin').diagnostics({ bufnr = 0, prompt_title = "Current file" })
+        end)
+
+        if status then
+            return
+        end
+
+        vim.diagnostic.setloclist()
+    end, },
+    { "n", "<leader>dqf", function()
+        local status = pcall(function()
+            require('telescope.builtin').diagnostics({ prompt_title = "Project" })
+        end)
+
+        if status then
+            return
+        end
+
+        vim.diagnostic.setqflist()
+    end, },
 }
