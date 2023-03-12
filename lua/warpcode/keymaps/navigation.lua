aab28db4958677109295bd453f18d1d7a6585951
Warpcode <warpcodeuk@gmail.com>
2023-03-12 15:45:16 +0000

    Formatting

--- lua/warpcode/keymaps/navigation.lua
+++ lua/warpcode/keymaps/navigation.lua
@@ -1,28 +1,28 @@
 return {
     -- Buffers
-    {'n', '<leader>bb', function()
+    { 'n', '<leader>bb', function()
         require('telescope.builtin').buffers()
-    end},
-    {'n', '<leader>bp', ':bp<CR>'},
-    {'n', '<leader>bn', ':bn<CR>'},
-    {'n', '<leader>bc', ':bd<CR>'},
-    {'n', '<leader>bq', ':bp <BAR> bd #<cr>'},
+    end },
+    { 'n', '<leader>bp', ':bp<CR>' },
+    { 'n', '<leader>bn', ':bn<CR>' },
+    { 'n', '<leader>bc', ':bd<CR>' },
+    { 'n', '<leader>bq', ':bp <BAR> bd #<cr>' },
 
     -- File Explorer
-    {'n', '<leader>nw', ':Lexplore<CR>'},
+    { 'n', '<leader>nw', ':Lexplore<CR>' },
 
     -- File Finder
-    {'n', '<leader>ff', function()
+    { 'n', '<leader>ff', function()
         require('warpcode.plugins.telescope.finders').project_files()
-    end},
+    end },
     -- Recent files
     -- File Finder
-    {'n', '<leader>fr', ':Telescope oldfiles cwd_only=true<CR>'},
+    { 'n', '<leader>fr', ':Telescope oldfiles cwd_only=true<CR>' },
 
     -- command! -nargs=1 FindFile call warpcode#search#findFiles(<q-args>)
 
     -- move vertically by visual line (ie will go into a wrapped line that's
     -- visually on the next line)
-    {'n', 'j', 'gj'},
-    {'n', 'k', 'gk'},
+    { 'n', 'j',          'gj' },
+    { 'n', 'k',          'gk' },
 }
