014d3cec6a54f9b68850dd92cba2e19a6732610a
Warpcode <warpcodeuk@gmail.com>
2023-03-17 20:45:35 +0000

    mechanism to load in a licence key for intelephense

--- lua/warpcode/lsp/lspconfig.lua
+++ lua/warpcode/lsp/lspconfig.lua
@@ -19,6 +19,21 @@ local servers = {
     {
         package = 'intelephense',
         lspconfig = 'intelephense',
+        config = function()
+            local conf = {
+                init_options = {}
+            }
+
+            local licenceKeyFile = vim.fn.expand('~/intelephense/licence.key')
+            local f = io.open(licenceKeyFile, "rb")
+            if f ~= nil then
+                -- Read a single line to get the key
+                conf.init_options.licenceKey = f:read "*l"
+                f:close()
+            end
+
+            return conf
+        end
     },
     {
         package = 'json-lsp',
