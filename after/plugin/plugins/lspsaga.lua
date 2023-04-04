6ec6d0b5f5e5e3ea152635c007f13fab43c1fa56
Warpcode <warpcodeuk@gmail.com>
2023-04-04 14:11:30 +0100

    lspsaga overrides

--- after/plugin/plugins/lspsaga.lua
+++ after/plugin/plugins/lspsaga.lua
@@ -1,3 +1,12 @@
 pcall(function()
+    local pexec = require 'warpcode.priority-exec'
     require "lspsaga".setup({})
+
+    -- LSP overrides
+    pexec.addCall('lsp.code_action', function () vim.cmd [[ Lspsaga code_action ]]  end, 10)
+    pexec.addCall('lsp.rename', function () vim.cmd [[ Lspsaga rename ]]  end, 10)
+
+    -- diagnostics overrides
+    pexec.addCall('diagnostic.next', function () require("lspsaga.diagnostic"):goto_next()  end, 10)
+    pexec.addCall('diagnostic.prev', function () require("lspsaga.diagnostic"):goto_prev()  end, 10)
 end)
