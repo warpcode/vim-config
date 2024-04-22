7ddfb3a029aad39511126bbb9deae12406810b5d
Warpcode <warpcodeuk@gmail.com>
2024-04-22 08:44:56 +0100

    Try catppuccin theme

--- lua/warpcode/plugins/plugin/themes.lua
+++ lua/warpcode/plugins/plugin/themes.lua
@@ -1,8 +1,16 @@
 return {
-    -- [[ Themes ]]
-    'RRethy/nvim-base16',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  -- [[ Themes ]]
+  'RRethy/nvim-base16',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  init = function()
+    -- vim.cmd.colorscheme('base16-gruvbox-dark-hard')
+  end,
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
     init = function()
-        vim.cmd.colorscheme 'base16-gruvbox-dark-hard'
+      vim.cmd.colorscheme('catppuccin-mocha')
     end,
+  },
 }
